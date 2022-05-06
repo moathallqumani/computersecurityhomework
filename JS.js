50130db726e85f7253792e4d55878375e76d31a8
Moath alluqmani <Moathalluqmani@gmail.com>
2022-05-06 14:20:51 +0300

    first commit

---
+++ JS.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCSlVB9CdThR_0-ybIjasLhZ9Mezi8fXYc",
+    authDomain: "pracitse.firebaseapp.com",
+    projectId: "pracitse",
+    storageBucket: "pracitse.appspot.com",
+    messagingSenderId: "150493266537",
+    appId: "1:150493266537:web:70db252562dfd1579518f8",
+    measurementId: "G-55K3K1N98Q"
+};
+import { GoogleAuthProvider } from "@firebase/auth";
+const provider = new GoogleAuthProvider();
